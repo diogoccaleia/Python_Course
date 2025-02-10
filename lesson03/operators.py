5df37ee19fb6f1e9cbc52309db6a6dc1ed372150
diogoccaleia <83734025+diogoccaleia@users.noreply.github.com>
2025-02-10 16:54:57 +0000

    Initial files upload

---
+++ lesson03/operators.py
@@ -0,0 +1,10 @@
+meaning = 3
+print('')
+
+#if meaning > 10:
+#    print('Right on!')
+#else:
+#    print('Not today')
+
+# ternary operator~
+print('Right on!') if meaning > 10 else print('Not today!')
