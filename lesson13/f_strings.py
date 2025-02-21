2a739440c99373cb596fe071be2e66005ff62134
diogoccaleia <83734025+diogoccaleia@users.noreply.github.com>
2025-02-21 07:11:52 +0000

    f-strings

---
+++ lesson13/f_strings.py
@@ -0,0 +1,45 @@
+person = "Diogo"
+coins = 3
+
+# print("\n" + person + " has " + str(coins) + " coins left.")
+
+# message = "\n%s has %s coins left." % (person, coins)
+# print(message)
+
+# message = "\n {} has {} coins left.".format(person, coins)
+# print(message)
+
+# message = "\n {1} has {0} coins left.".format(person, coins)
+# print(message)
+
+player = {'person': 'Diogo', 'coins': 3}
+# message = "\n{person} has {coins} coins left.".format(**player)
+# print(message)
+
+#################################
+# f-strings
+
+message = f"\n{person} has {coins} coins left."
+print(message)
+
+message = f"\n{person} has {2*5} coins left."
+print(message)
+
+message = f"\n{person.lower()} has {2*5} coins left."
+print(message)
+
+message = f"\n{player['person']} has {2*5} coins left."
+print(message)
+
+############
+# passing formatting options
+
+num = 10
+print(f"\n2.25 times {num} is {2.25 * num:.2f}\n")
+
+for num in range(1, 11):
+    print(f"2.25 times {num} is {2.25 * num:.2f}")
+
+for num in range(1, 11):
+    print(f"{num} divided by 4.52 is {num / 4.52:.2%}")
+
